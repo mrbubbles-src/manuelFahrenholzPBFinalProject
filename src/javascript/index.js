04b64b0cf33c917c79d6b61fddb544bf3dff8964
mrbubbles-src <feaver5683@gmail.com>
2023-02-21 11:10:49 +0100

    created second test pokemon

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -11,5 +11,6 @@ class Pokemon {
     getHealth() {}
 }
 const pikachu = new Pokemon("Pikachu", 100, 50);
-
 console.log(pikachu);
+const bulbasaur = new Pokemon("Bulbasaur", 90, 60);
+console.log(bulbasaur);
