251ac521dd77774ceb35b0e1a5cb27487e19e55e
mrbubbles-src <feaver5683@gmail.com>
2023-02-21 16:58:47 +0100

    attack done; getHealth & getMagic in progress

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -25,14 +25,33 @@ HP: ${this.health}
 MP: ${this.magic}
 Available Skills: ${availableSkills.join(", ")}`;
     }
-    attack() {}
-    getMagic() {}
-    getHealth() {}
+    // mana prüfung umdrehen
+    attack(skillIndex, attackedPokemon) {
+        if (this.magic < this.skills[skillIndex].MPCost) {
+            return `${this.name} only has ${this.magic}MP left and can't use ${this.skills[skillIndex].skillName}.`;
+        } else if (this.magic >= this.skills[skillIndex].MPCost) {
+            this.magic -= this.skills[skillIndex].MPCost;
+            attackedPokemon.health -= this.skills[skillIndex].dmgAmount;
+            if (attackedPokemon.health <= 0) {
+                attackedPokemon.health = 0;
+                return `${this.name} used ${this.skills[skillIndex].skillName} against ${attackedPokemon.name}. ${attackedPokemon.name} fainted! ${this.name} has won the battle!`;
+            }
+            return `${this.name} used ${this.skills[skillIndex].skillName} against ${attackedPokemon.name} and dealt ${this.skills[skillIndex].dmgAmount} damage! ${attackedPokemon.name} has ${attackedPokemon.health} HP left`;
+        }
+    }
+    getMagic() {
+        this.magic += 20;
+        return `${this.name} used a Potion and restored 20MP. ${this.name} noch has ${this.magic}`;
+    }
+    getHealth() {
+        this.health += 50;
+        return `${this.name} used a Potion and restored 50HP. ${this.name} noch has ${this.health}`;
+    }
     learnAttackSkill(skillStr) {
         if (skillStr) this.skills.push(skillStr);
     }
 }
-const pikachu = new Pokemon("Pikachu", 100, 50);
+const pikachu = new Pokemon("Pikachu", 100, 210);
 // console.log(pikachu);
 const bulbasaur = new Pokemon("Bulbasaur", 90, 60);
 // console.log(bulbasaur);
@@ -55,5 +74,13 @@ bulbasaur.learnAttackSkill(tackle);
 // console.log(pikachu);
 // console.log(bulbasaur.skills[0].skillName);
 // console.log(bulbasaur);
+// console.log(pikachu.showStatus());
+console.log(pikachu.attack(0, bulbasaur));
 console.log(bulbasaur.showStatus());
 console.log(pikachu.showStatus());
+console.log(pikachu.attack(0, bulbasaur));
+console.log(bulbasaur.showStatus());
+console.log(pikachu.showStatus());
+console.log(pikachu.attack(0, bulbasaur));
+console.log(bulbasaur.getMagic());
+console.log(bulbasaur.showStatus());
