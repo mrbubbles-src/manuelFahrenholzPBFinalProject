a988a232398b5877921805c2942a69315dc868da
mrbubbles-src <feaver5683@gmail.com>
2023-02-21 13:52:40 +0100

    learnAttackSkill done

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,3 +1,6 @@
+/**
+ * POKEMON CLASS
+ */
 class Pokemon {
     constructor(name, health, magic) {
         this.name = name;
@@ -13,11 +16,32 @@ MP: ${this.magic}`;
     attack() {}
     getMagic() {}
     getHealth() {}
+    learnAttackSkill(skillStr) {
+        if (skillStr) this.skills.push(skillStr);
+    }
 }
 const pikachu = new Pokemon("Pikachu", 100, 50);
 console.log(pikachu);
+console.log(pikachu.showStatus());
 const bulbasaur = new Pokemon("Bulbasaur", 90, 60);
 console.log(bulbasaur);
-
-console.log(pikachu.showStatus());
 console.log(bulbasaur.showStatus());
+
+/**
+ * ATTACKSKILL CLASS
+ */
+class AttackSkill {
+    constructor(skillName, dmgAmount, MPCost) {
+        this.skillName = skillName;
+        this.dmgAmount = dmgAmount;
+        this.MPCost = MPCost;
+    }
+}
+const lightningBolt = new AttackSkill("Lightning Bolt", 40, 70);
+const tackle = new AttackSkill("Tackle", 20, 30);
+pikachu.learnAttackSkill(lightningBolt);
+bulbasaur.learnAttackSkill(tackle);
+// console.log(pikachu.skills[0].skillName);
+console.log(pikachu);
+// console.log(bulbasaur.skills[0].skillName);
+console.log(bulbasaur);
